f35cb84ded1d3bb018f9d5cdd1213edbea115ce1
h42m0ny <h42m0ny@gmail.com>
2020-10-21 14:13:26 +0200

    correct order of display message in testCase function

--- tests/lib.js
+++ tests/lib.js
@@ -1,33 +1,33 @@
 function assert(message, expr) {
-    if (!expr) {
-        output(false, message);
-        throw new Error(message);       
-    }
-    output(true, message);
+  if (!expr) {
+    output(false, message);
+    throw new Error(message);
+  }
+  output(true, message);
 }
 
-function output(result, message){
-    message += result ? ' : SUCCESS' : ' : FAILURE';
-    console.log(message);
+function output(result, message) {
+  message += result ? " : SUCCESS" : " : FAILURE";
+  console.log(message);
 }
 
-function testCase(message, tests){
-    let total = 0;
-    let succeed = 0;
-    for(test in tests){
-     total++;
-     try{
+function testCase(message, tests) {
+  console.log(message);
+  let total = 0;
+  let succeed = 0;
+  for (test in tests) {
+    total++;
+    try {
       tests[test]();
       succeed++;
-     }catch(err){  
-     }
-    }
-    
-    let testsResult = 'succeed tests ' + succeed + '/' + total ;
-    console.log(`${message}\n${testsResult}`);
+    } catch (err) {}
+  }
+
+  let testsResult = "succeed tests " + succeed + "/" + total;
+  console.log(testsResult);
 }
 
 module.exports = {
-    assert: assert,
-    testCase: testCase
+  assert: assert,
+  testCase: testCase,
 };
