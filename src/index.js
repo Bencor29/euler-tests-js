dfcb7372af008f752c028e572c25ee0f12ce8499
h42m0ny <h42m0ny@gmail.com>
2020-10-21 13:11:22 +0200

    add function sumMultiples to resolve challeng 1 of euler project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function sumMultiples(limit){
+    let sumMult = 0;
+    
+    for (let i=0; i < limit; i++){
+        if( (i % 3 === 0) || (i % 5 === 0)) sumMult += i;
+    }
+    
+    return sumMult;
+
+}
