4c1e01446206e3d330e3e0781353ec6e118ce4c6
h42m0ny <h42m0ny@gmail.com>
2020-10-21 13:44:52 +0200

    add parameters for multiple, cond to check limit is up to 0 and first test case

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,9 @@
-// import('./lib.js')
 let ut = require('./lib.js');
+let ind = require('../src/index.js')
 
-ut.assert('Ceci est le message d\'erreur', false);
+
+ut.testCase('Test sum multiples',{
+   'test without multiples parameters and limit 1000':  function(){
+       ut.assert('Should return 233168',ind.sumMultiples(1000) === 233168)
+   }
+});
--- tests/lib.js
+++ tests/lib.js
@@ -1,9 +1,33 @@
 function assert(message, expr) {
     if (!expr) {
-        throw new Error(message);
+        output(false, message);
+        throw new Error(message);       
     }
+    output(true, message);
+}
+
+function output(result, message){
+    message += result ? ' : SUCCESS' : ' : FAILURE';
+    console.log(message);
+}
+
+function testCase(message, tests){
+    let total = 0;
+    let succeed = 0;
+    for(test in tests){
+     total++;
+     try{
+      tests[test]();
+      succeed++;
+     }catch(err){  
+     }
+    }
+    
+    let testsResult = 'succeed tests ' + succeed + '/' + total ;
+    console.log(`${message}\n${testsResult}`);
 }
 
 module.exports = {
-    assert: assert
+    assert: assert,
+    testCase: testCase
 };
